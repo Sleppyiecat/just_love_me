d49a2b0da89041d5220c0a81fd3196373303298d
Sleppyiecat <mruczek70@gmail.com>
2021-10-10 12:42:20 +0000

    VantomBE: Implement automated build scripts * Usefull especially on scheduled builds * Makes compiling lot easier

---
+++ zipme.sh
@@ -0,0 +1,14 @@
+            echo "_________________________________________________"
+	    echo "=                    VantomBE                   ="
+	    echo "=                Developer: Karou               ="
+	    echo "=                                               ="
+	    echo "=          Have i said that i love you?         ="
+	    echo "=---------------------------------------------- ="
+	    echo "=     > Generating flashable zip                ="
+	    echo -e "=_______________________________________________="
+
+            git clone https://github.com/Sleppyiecat/Anykernel --depth=1 out/arch/arm64/boot/ZIP
+	    rm -rf out/arch/arm64/boot/ZIP/.git
+            cp -rf out/arch/arm64/boot/Image.gz-dtb out/arch/arm64/boot/ZIP/Image.gz-dtb
+	    cp -rf out/arch/arm64/boot/dtbo.img out/arch/arm64/boot/ZIP/dtbo.img
+            cd out/arch/arm64/boot/ZIP && zip -r Kernel.zip ./
